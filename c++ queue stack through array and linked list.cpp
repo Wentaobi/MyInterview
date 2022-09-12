adace0251a609b78d3ac9e23158370098279bb3b
Wentao Bi <wentao.bi@gatech.edu>
2022-09-11 17:48:53 -0700

    Update and rename c++ queue through array and linked list.cpp to c++ queue stack through array and linked list.cpp

--- c++ queue stack through array and linked list.cpp
+++ c++ queue stack through array and linked list.cpp
@@ -1,5 +1,9 @@
 // C++ queue
 // Array
+// head tail lock
+// fine-grain lock linked list
+// pthread_mutex_init(n, NULL);
+// lock free - atomic(linked list node) memory barrier
 template<typename Type>
 class Queue {
 public:
@@ -91,10 +95,66 @@ struct ring_buffer push(struct ring_buffer& b, int data) {
     b.end = b.end->next;
     return b;
 }
-
 struct ring_buffer pop(struct ring_buffer& b) {
     int tmp = b.start->data;
     b.start->empty = 1;
     b.start = b.start->next;
     return b;
 }
+
+// Stack
+template<typename Type>
+class Stack {
+public:
+    virtual void push(Type& element) = 0;   // push element into stack
+    virtual void pop() = 0;                 // top element from stack
+    virtual Type top() = 0;                 // get first element from stack
+    virtual void clear() = 0;               // clear stack
+    virtual int size() = 0;                 // get stack size 
+};
+template<typename Type, int N>
+class staticStack : public Stack<Type> {
+protected:
+    Type m_space[N];
+    int m_top;
+    int m_length;
+public:
+    staticStack():m_top(0),  m_length(0) {}
+    int capacity() {return N;}
+    void push(Type& element) override {
+        if (m_length < N) {
+            m_space[m_length] = element;
+            m_top++;
+            m_length++;
+        }
+        else {"throw error";}
+    }
+    void pop() override {
+        if (m_length > 0) {
+            int tmp = m_space[m_top];
+            m_top--;
+            m_length--;
+        }
+    }
+    Type top() const override {
+        if (m_length > 0)
+            return m_space[m_top];
+    }
+    void clear() override {
+        m_top = 0;
+        m_length = 0;
+    }
+    int size() override {
+        return m_length;
+    }
+}; 
+// prev_prev prev curr next
+// size() ---> len
+// push() : curr->next = node(data, null); lookup[node] = curr;  curr=node;
+// pop()  : int tmp = curr->data; lookup[curr]->next=nullptr; lookup[curr]=nullptr; delete(curr); 
+// unordered_map<Node*, Node*> lookup;
+// prev = lookup[curr]
+// prev_prev = lookup[prev]
+// empty(): head == curr?, len check
+// clear(): tail->head, delete(curr); curr=null; 
+
